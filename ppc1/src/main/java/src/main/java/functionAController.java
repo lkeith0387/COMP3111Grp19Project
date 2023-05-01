383a2c340202566cf217f5e65eb76b510b82af83
AL-root <125720792+AL-root@users.noreply.github.com>
2023-05-01 12:09:44 +0800

    Add files

---
+++ ppc1/src/main/java/src/main/java/functionAController.java
@@ -0,0 +1,179 @@
+package src.main.java;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
+
+public class functionAController {
+
+    @FXML
+    private TextField Cap_Grape;
+
+    @FXML
+    private TextField Cap_Labor;
+
+    @FXML
+    private TextField Fixed_Costs;
+
+    @FXML
+    private Line HLine2;
+
+    @FXML
+    private Line HLine3;
+
+    @FXML
+    private Line HLine4;
+
+    @FXML
+    private Line HLine_1;
+
+    @FXML
+    private TextField Num_Week;
+
+    @FXML
+    private TextField Prc_Noir;
+
+    @FXML
+    private TextField Prc_Rose;
+
+    @FXML
+    private Line VLine1;
+
+    @FXML
+    private Line VLine2;
+
+    @FXML
+    private Button button_exit1;
+
+    @FXML
+    private Button button_run1;
+
+    @FXML
+    private Label data_input_caption1;
+
+    @FXML
+    private Label data_input_caption2;
+
+    @FXML
+    private Label data_input_caption2a;
+
+    @FXML
+    private Label data_input_caption2b;
+
+    @FXML
+    private Label data_input_caption3;
+
+    @FXML
+    private Label data_input_caption3a;
+
+    @FXML
+    private Label data_input_caption3b;
+
+    @FXML
+    private Label data_input_caption4;
+
+    @FXML
+    private Label data_input_heading1;
+
+    @FXML
+    private Label form_heading1;
+
+    @FXML
+    private AnchorPane label_col_heading2;
+
+    @FXML
+    private Label label_sysmsg1;
+
+    @FXML
+    private Label opt_r_caption1;
+
+    @FXML
+    private Label opt_r_caption1a;
+
+    @FXML
+    private Label opt_r_caption1b;
+
+    @FXML
+    private Label opt_r_caption1c;
+
+    @FXML
+    private Label opt_r_caption2;
+
+    @FXML
+    private Label opt_r_caption3;
+
+    @FXML
+    private Label opt_result_heading1;
+
+    @FXML
+    private TextField or_Gross_Profit;
+
+    @FXML
+    private TextField or_Prod_Vol_Noir;
+
+    @FXML
+    private TextField or_Prod_Vol_Rose;
+
+    @FXML
+    private TextField or_Prod_Vol_Total;
+
+    @FXML
+    private TextField or_Profit_Margin;
+
+    @FXML
+    private Spinner<String> or_scroll_text1;
+
+    @FXML
+    private Line vLine3;
+
+    @FXML
+    void buttonpressed(ActionEvent event) {
+
+    }
+    public void initialize() {
+        Num_Week.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                try {
+                    if (0 > Integer.parseInt(newValue) || 15 < Integer.parseInt(newValue)) {
+                        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                        alert.setTitle("error");
+                        alert.setHeaderText("input error");
+                        alert.setContentText("Please enter an integer greater than 0 and less than 15");
+                        alert.showAndWait();
+                        Num_Week.setText("");
+                    } else {
+
+                    }
+                } catch (NumberFormatException e) {
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("error");
+                    alert.setHeaderText("input error");
+                    alert.setContentText("Please enter the number greater than 0 and less than 15");
+                    alert.showAndWait();
+                    Num_Week.setText("");
+                }
+            }
+        });
+    }
+    @FXML
+    private void getDefaultValue(InputMethodEvent event) {
+
+
+    }
+
+    public void toexit(ActionEvent actionEvent) {Main.stage.setScene(Main.scene);
+    }
+
+    public void toclick(ActionEvent actionEvent) {
+        ObservableList<String> items = FXCollections.observableArrayList("w1: Insufficient production capacity to produce the optimal mix, please reduce or adjust the capacity of labor & grape volume!", "W2: Insufficient labor supplied to utilize the grape resource (less than 90%).");
+        or_scroll_text1.setValueFactory(new SpinnerValueFactory.ListSpinnerValueFactory<String>(items));
+    }
+}
