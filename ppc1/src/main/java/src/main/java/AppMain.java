07574b75aa7c8674d8ed0de3657f262b89d64ec5
AL-root <125720792+AL-root@users.noreply.github.com>
2023-05-02 00:54:18 +0800

    Update

--- ppc1/src/main/java/src/main/java/AppMain.java
+++
@@ -1,9 +0,0 @@
-package src.main.java;
-
-import javafx.application.Application;
-
-public class AppMain {
-    public static void main(String[] args) {
-        Application.launch(Main.class,args);
-    }
-}
