d37aad83b4e3dc376eccff836ee3a66418504bb4
AL-root <125720792+AL-root@users.noreply.github.com>
2023-05-04 00:16:41 +0800

    Update function layout

---
+++ ppc1/src/main/java/src/main/java/AppMain.java
@@ -0,0 +1,9 @@
+package src.main.java;
+
+import javafx.application.Application;
+
+public class AppMain {
+    public static void main(String[] args) {
+        Application.launch(Main.class,args);
+    }
+}
